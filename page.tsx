e6dd2c31298c8eef74b1810dc610b814736c626e
Haasnain Raza <170979432+hasnainraza321@users.noreply.github.com>
2024-10-28 01:07:45 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Helloword = () => {
+  return (
+    <div>
+      <h1 className='text-xl font-bold underline text-center m-20'>Hello word</h1>
+    </div>
+  )
+}
+
+export default Helloword
